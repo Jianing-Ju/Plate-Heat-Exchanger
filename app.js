a3f79c70d32a3f18cfe96f7f3349efeda56ab650
jujianing <jujialemon@gmail.com>
2022-03-30 12:09:07 +0800

    For Heroku

--- app.js
+++ app.js
@@ -24,7 +24,13 @@ const paths = {
 }
 
 // CORS
-app.use(cors());
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 // serve static files
 app.use(express.static('public'))
--- controller/utils/db.js
+++ controller/utils/db.js
@@ -1,11 +1,19 @@
 import { createPool } from "mysql";
 const pool = createPool({
-    host: "localhost",
-    user: "root",
-    password: "Olivia@03",
-    database: "db_PHE",
+    host: "us-cdbr-east-05.cleardb.net",
+    user: "bb1b355d0e0280",
+    password: "984f1405",
+    database: "heroku_a420b86963381b8",
     connectionLimit: 10
 })
+// const pool = createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "Olivia@03",
+//     database: "db_PHE",
+//     connectionLimit: 10
+// })
+
 
 export function query(sql, args){
     return new Promise((resolve, reject)=>{
